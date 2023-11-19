415c9d0230dcc24b7f7a09909705f7414caee01c
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-11-19 18:36:31 +0100

    Added api key config.

---
+++ config/api_key.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Basic Authentication
+    |--------------------------------------------------------------------------
+    |
+    | This options are used by the middleware to check if the user has to
+    | authenticate using basic authentication.
+    |
+    */
+
+    'api_key' => env('API_KEY', false),
+
+];
